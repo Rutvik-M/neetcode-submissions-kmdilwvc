545d376db059d1a90f3b54e9ef0424471f7bd3c0
Rutvik-M <manthalkar.rutvik@gmail.com>
2026-04-03 20:05:37 +0530

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        hashmap={")":"(","]":"[","}":"{"}
+        for i in s:
+                if i in hashmap:
+                        top=stack.pop() if stack else 0
+                        if hashmap[i] !=top:
+                                return False
+                else:
+                        stack.append(i)
+        return len(stack)==0
+                
+        
