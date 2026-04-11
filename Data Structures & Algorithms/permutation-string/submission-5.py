cb3e41e4e87685ba657ed467719ff7ab1b4b302d
Rutvik-M <manthalkar.rutvik@gmail.com>
2026-04-11 14:28:49 +0530

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1)>len(s2):
+            return False
+        hashmap1={}
+        for i in s1:
+            hashmap1[i]=hashmap1.get(i,0)+1
+        hashmap2={}
+        left=0
+        for right in range(len(s2)):
+            char_in=s2[right]
+            hashmap2[char_in]=hashmap2.get(char_in,0)+1
+            if right-left+1>len(s1):
+                char_out=s2[left]
+                hashmap2[char_out]-=1
+                if hashmap2[char_out]==0:
+                    del hashmap2[char_out]
+                left+=1
+            if hashmap1==hashmap2:
+                return True
+        return False
+s1 = "abc"
+s2 = "lecabee"
+obj=Solution()
+res=obj.checkInclusion(s1,s2)
+print (res)
+
+        
